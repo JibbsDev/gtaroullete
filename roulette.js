d7de499d02d5ef753fcc406a501e6de14102c2f6
Jibbs Dev <68401777+JibbsDev@users.noreply.github.com>
2024-01-06 01:05:50 +0100

    Update roulette.js

--- roulette.js
+++ roulette.js
@@ -15,7 +15,7 @@ class RouletteBetting {
                 numbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
                 break;
             case 'green':
-                numbers = [0];
+                numbers = [0,00];
                 break;
             case '1st':
                 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
